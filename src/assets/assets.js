5be7698e7ac3731a3115e461c25bc6f99ab621ed
Hülya Nergiz <akin.hulya@outlook.com>
2023-12-02 23:57:05 +0300

    imports images to assets.js

---
+++ src/assets/assets.js
@@ -0,0 +1,37 @@
+/*Profile Photos*/
+import profilePhotoHeader from "./photos/photo-1.png";
+import profilePhotoInProfile from "./photos/photo-2.png";
+
+/*Skills*/
+import javaScriptLogo from "./skills/js-logo.png";
+import reactLogo from "./skills/react-logo.png";
+import reduxLogo from "./skills/redux-logo.png";
+import nodeLogo from "./skills/node-logo.png";
+import visualStudioLogo from "./skills/vs-code-logo.png";
+import figmaLogo from "./skills/figma-logo.png";
+
+/*Socials*/
+import github from "./socials/github.svg";
+import linkedin from "./socials/linkedin.svg";
+import twitter from "./socials/twitter.svg";
+import codepen from "./socials/codepen.svg";
+import atsign from "./socials/at-sign.svg";
+import instagram from "./socials/instagram.svg";
+
+/*Projects*/
+import workintech from "./projects/journey.png";
+import journey from "./projects/workintech.png";
+
+export const assets = {
+  photos: { profilePhotoHeader, profilePhotoInProfile },
+  skills: {
+    javaScriptLogo,
+    reactLogo,
+    reduxLogo,
+    nodeLogo,
+    visualStudioLogo,
+    figmaLogo,
+  },
+  socials: { github, linkedin, twitter, codepen, atsign, instagram },
+  projects: { workintech, journey },
+};

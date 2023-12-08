afd0f00a7a94c3ea360f599ba8b8254ddb93a110
Hülya Nergiz <akin.hulya@outlook.com>
2023-12-08 13:31:30 +0300

    adds spinner

--- src/App.js
+++ src/App.js
@@ -4,14 +4,27 @@ import Profile from "./components/Profile";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import "./App.css";
+import { useContext } from "react";
+import { FetchContext } from "./contexts/FetchContext";
+import { PulseLoader } from "react-spinners";
 function App() {
+  const { fetch } = useContext(FetchContext);
   return (
     <div className="App">
-      <Header />
-      <Skills />
-      <Profile />
-      <Projects />
-      <Footer />
+      {!fetch && (
+        <div classame="spinner">
+          <PulseLoader className="pulseLoader" />
+        </div>
+      )}
+      {fetch && (
+        <div>
+          <Header />
+          <Skills />
+          <Profile />
+          <Projects />
+          <Footer />
+        </div>
+      )}
     </div>
   );
 }
